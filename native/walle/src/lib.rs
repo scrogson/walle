ad0492af0dc4fc0fcc05bcdd89beacb557a9bfab
Sonny Scroggin <sonny@scrogg.in>
2024-04-02 17:00:41 -0500

    Add support for signing

--- native/walle/src/lib.rs
+++ native/walle/src/lib.rs
@@ -1,9 +1,28 @@
 use ethers::{
-    signers::Wallet,
+    core::{types::transaction::eip712::TypedData, utils::to_checksum},
+    signers::{Signer, Wallet},
     types::{Address, Signature},
+    utils::hex,
 };
+use once_cell::sync::Lazy;
+use std::future::Future;
 use std::io::Write;
 use std::str::FromStr;
+use tokio::runtime::{Builder, Runtime};
+
+static TOKIO: Lazy<Runtime> = Lazy::new(|| {
+    Builder::new_current_thread()
+        .build()
+        .expect("Walle: Failed to start tokio runtime")
+});
+
+pub fn block_on<F: Future>(future: F) -> F::Output
+where
+    F: Future + Send + 'static,
+    F::Output: Send + 'static,
+{
+    TOKIO.block_on(future)
+}
 
 #[rustler::nif]
 fn recover(message: String, signature: String) -> Result<String, String> {
@@ -47,7 +66,63 @@ fn decrypt_keystore(keystore: String, password: String) -> Result<String, String
     Ok(format!("{:x}", wallet.signer().to_bytes()))
 }
 
+#[rustler::nif(schedule = "DirtyCpu")]
+fn public_address(private_key: String) -> Result<String, String> {
+    let wallet = Wallet::from_bytes(&hex::decode(&private_key).unwrap()).unwrap();
+    Ok(to_checksum(&wallet.address(), None))
+}
+
+#[rustler::nif(schedule = "DirtyIo")]
+fn sign_message(message: String, private_key: String) -> Result<String, String> {
+    let signature = block_on(async move {
+        let wallet = Wallet::from_bytes(&hex::decode(&private_key).unwrap()).unwrap();
+        wallet.sign_message(message.as_bytes()).await
+    })
+    .unwrap();
+    Ok(format!("0x{}", signature.to_string()))
+}
+
+#[rustler::nif(schedule = "DirtyIo")]
+fn sign_typed_data(typed_data: String, private_key: String) -> Result<String, String> {
+    let typed_data: TypedData = match serde_json::from_str(&typed_data) {
+        Ok(typed_data) => typed_data,
+        Err(e) => return Err(e.to_string()),
+    };
+
+    let signature = block_on(async move {
+        let wallet = Wallet::from_bytes(&hex::decode(&private_key).unwrap()).unwrap();
+        wallet.sign_typed_data(&typed_data).await
+    })
+    .unwrap();
+
+    Ok(format!("0x{}", signature.to_string()))
+}
+
+#[rustler::nif(schedule = "DirtyCpu")]
+fn recover_typed_data(typed_data: String, signature: String) -> Result<String, String> {
+    let typed_data: TypedData = match serde_json::from_str(&typed_data) {
+        Ok(typed_data) => typed_data,
+        Err(e) => return Err(e.to_string()),
+    };
+
+    let signature = Signature::from_str(&signature).map_err(|e| e.to_string())?;
+
+    match signature.recover_typed_data(&typed_data) {
+        Ok(address) => Ok(format!("{:#x}", address)),
+        Err(e) => Err(e.to_string()),
+    }
+}
+
 rustler::init!(
     "Elixir.Walle",
-    [recover, verify, new_keystore, decrypt_keystore]
+    [
+        recover,
+        verify,
+        new_keystore,
+        decrypt_keystore,
+        public_address,
+        sign_message,
+        sign_typed_data,
+        recover_typed_data
+    ]
 );
--- test/walle_test.exs
+++ test/walle_test.exs
@@ -14,6 +14,10 @@ defmodule WalleTest do
        address: "0x70997970c51812dc3a010c7d01b50e0d17dc79c8",
        signature:
          "3e5991581f15944dce56238e3efe40bbeb383c7e768b9847646e91be70c80a322a24b735fcfe81b2ef72ea5c67566a5b4b5d197cf65cf105a343d6393820a9d91b"
+     },
+     private: %{
+       private_key: "0b50ebd4eec51a753db1e8af2ab44c83c867dfe10f2d677cc4183cf1570e5585",
+       address: "0x63d8090c7353a57485965251506dd2a15244dee5"
      }}
   end
 
@@ -76,7 +80,82 @@ defmodule WalleTest do
     test "decrypts the wallet JSON keystore" do
       password = "password"
       assert {:ok, keystore} = Walle.new_keystore(password)
-      assert {:ok, _private_key} = Walle.decrypt_keystore(keystore, password)
+      assert {:ok, private_key} = Walle.decrypt_keystore(keystore, password)
+      assert String.length(private_key) == 64
     end
+  end
+
+  describe "public_address/1" do
+    test "returns the public address from the private key", %{private: private} do
+      assert {:ok, address} = Walle.public_address(private.private_key)
+
+      assert String.downcase(address) == private.address
+    end
+  end
+
+  describe "sign_message/2" do
+    test "signs the message with the private key", %{message: message, private: private} do
+      %{address: address, private_key: private_key} = private
+
+      {_micros, {:ok, signature}} = :timer.tc(Walle, :sign_message, [message, private_key])
+      # IO.inspect({micros, signature})
+      # 218 microseconds (milliseconds: 0.218)
+      assert String.length(signature) == 132
+      assert Walle.recover(message, signature) == {:ok, address}
+    end
+  end
+
+  describe "sign_typed_data/2" do
+    test "signs the EIP-721 message with the private key", %{private: private} do
+      %{address: address, private_key: private_key} = private
+
+      typed_data = eip_712_message()
+
+      {:ok, signature} = Walle.sign_typed_data(typed_data, private_key)
+      assert String.length(signature) == 132
+
+      assert {:ok, ^address} = Walle.recover_typed_data(typed_data, signature)
+    end
+  end
+
+  defp eip_712_message do
+    ~s({
+      "types": {
+        "EIP712Domain": [
+          {
+            "name": "name",
+            "type": "string"
+          },
+          {
+            "name": "version",
+            "type": "string"
+          },
+          {
+            "name": "chainId",
+            "type": "uint256"
+          },
+          {
+            "name": "verifyingContract",
+            "type": "address"
+          }
+        ],
+        "Message": [
+          {
+            "name": "data",
+            "type": "string"
+          }
+        ]
+      },
+      "primaryType": "Message",
+      "domain": {
+        "name": "example.metamask.io",
+        "version": "1",
+        "chainId": "1",
+        "verifyingContract": "0x0000000000000000000000000000000000000000"
+      },
+      "message": {
+        "data": "Hello!"
+      }
+    })
   end
 end
