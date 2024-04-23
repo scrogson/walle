7e63e13c33796c82456da4ab36b4b7dcd0e667c9
Sonny Scroggin <sonny@scrogg.in>
2024-04-23 06:57:46 -0500

    Add checksum

---
+++ checksum-Elixir.Walle.Native.exs
@@ -0,0 +1,11 @@
+%{
+  "libwalle-v0.5.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:bc019f28c2842c7447b35396fe60c1ac8aa9b2e3738fae1908883f11bde1a1c1",
+  "libwalle-v0.5.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:e155e1424db9ed97dc5f0c6a5108172f06313642d1a4151920b019efdcfe403a",
+  "libwalle-v0.5.0-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:88225ae3f9feebc38d8077163e953d7eb8d3fb081bbe6a5e72386e6624699c10",
+  "libwalle-v0.5.0-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:af0f6904f9b14a7a63a20c510c30b8a2150127f418c60d63060242ed982e72c7",
+  "libwalle-v0.5.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:02ea5eb28de62edbf586d292db77d4d2c6a34d9b9f810c71c4f0f66cf2d37f74",
+  "libwalle-v0.5.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:541b910ac0286ee21096c259767ab08d495cc3075acb8e1526c2958569c23312",
+  "libwalle-v0.5.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:8668ec7b0bca4bf6b913802c59ea615d4642223e98aaeb3380eefb409aa79799",
+  "walle-v0.5.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:48587ed7ce20cd6f6c86486484197620a4386a6f3d6cc9f99a91fece27bd5c48",
+  "walle-v0.5.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:ba8d1eddb2370bc4db19c2eac273a05f84c022b605d9cb2db05f59dd502f9e04",
+}
